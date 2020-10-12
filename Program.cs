6c920f75d75b459730e7ef39006edffbee4618d9
Edgar Espadron <43763091+edgarespadron@users.noreply.github.com>
2020-10-12 01:06:38 -0500

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace FundOne
+{
+    class Program
+    {
+        static void result(int N)
+        {
+            for (int num = 0; num < N; num++)
+            {
+                if (num % 3 == 0 && num % 5 == 0)
+                Console.WriteLine(num + "fizzbuzz");
+
+                else if (num % 3 == 0)
+                Console.WriteLine(num + "fizz");
+
+                else if (num % 5 == 0)
+                Console.WriteLine(num + "buzz");
+                
+            }
+                
+        }
+        static void Main(){
+            int N = 100;
+            result(N);
+        }
+    }
+}
+        
+           
